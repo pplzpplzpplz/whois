8e533dcfa8d6bbaba3c7557a36737180679fb50c
calebrauxa <moore.caleb@gmail.com>
2022-05-09 17:33:23 -0700

    autocomplete hooked up

--- js/script.js
+++ js/script.js
@@ -4,12 +4,18 @@ const playButton = document.querySelector('#playButton');
 const skipButton = document.querySelector('#skipButton');
 const turnsCount = document.querySelector('#turnsCount');
 const listensCount = document.querySelector('#listensCount');
+const timeCount = document.querySelector('#timeCount');
+const audioIncrement = .1;
+const countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+
 let song;
 let cueStart = 43.5;
 let duration = .1;
 
 let turnsCountData = 0;
 let listensCountData = 0;
+let timeCountData = .1;
 let finalScore;
 
 function preload() {
@@ -30,11 +36,9 @@ playButton.addEventListener('click', playSound);
 
 function playSound() {
   gsap.to(playButton, {duration: .05, opacity: 0});
-  // play([startTime], [rate], [amp], [cueStart], [duration])
-  // cueStart: how far into the song should it start
-  // duration: how long should it play for
   listensCountData = listensCountData + 1;
   listensCount.innerHTML = listensCountData;
+  // play([startTime], [rate], [amp], [cueStart], [duration])
   song.play(0, 1, 1, cueStart, duration);
   song.onended(function() {
     gsap.to(playButton, {duration: .05, opacity: 1});
@@ -46,7 +50,9 @@ skipButton.addEventListener('click', skipClicked);
 function skipClicked() {
   turnsCountData = turnsCountData + 1;
   turnsCount.innerHTML = turnsCountData;
-  duration = duration + .1;
+  duration = duration + audioIncrement;
+  timeCountData = timeCountData + audioIncrement
+  timeCount.innerHTML = `${timeCountData.toFixed(1)} s`;
 }
 
 
@@ -108,11 +114,8 @@ document.addEventListener('keypress', function(e) {
       document.querySelector('.winner').classList.add('show');
       document.querySelector('iframe').classList.add('show');
       
-      if (turnsCountData === 0 || listensCountData === 0) {
-        finalScore = turnsCountData + listensCountData;
-      } else {
-      finalScore = turnsCountData + listensCountData;
-      }
+
+      finalScore = turnsCountData + listensCountData + timeCountData;
       
       document.querySelector('#score').innerHTML = `${finalScore}`;
       let winnerSongID = '4LRPiXqCikLlN15c3yImP7';
